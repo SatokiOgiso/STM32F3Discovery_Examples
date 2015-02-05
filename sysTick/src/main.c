ffa4d0a206f59bf4320b6570078deaa7975c099a
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-02-05 16:54:44 +0900

    added an example to use sysTick

---
+++ sysTick/src/main.c
@@ -0,0 +1,68 @@
+/*
+**
+**                           Main.c
+**
+**
+**********************************************************************/
+/*
+   Last committed:     $Revision: 00 $
+   Last changed by:    $Author: $
+   Last changed date:  $Date:  $
+   ID:                 $Id:  $
+
+**********************************************************************/
+#include <stm32f30x_conf.h>
+/* Private variables ---------------------------------------------------------*/
+RCC_ClocksTypeDef RCC_Clocks;
+uint32_t sysTickFlag = 0;
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+	sysTickFlag = 1;
+}
+
+void init(void)
+{
+	GPIO_InitTypeDef	GPIO_InitStructure;
+
+	//Provide clock to GPIO
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
+	//Initialize GPIO
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;  // Output
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP; // Push Pull
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOE, &GPIO_InitStructure);
+}
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /* SysTick end of count event each 100ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 10);
+
+  /* Initialize LEDs and User Button available on STM32F3-Discovery board */
+
+  init();
+
+  /* Infinite loop */
+  while (1)
+  {
+		if(sysTickFlag == 1){
+			sysTickFlag = 0;
+			GPIO_Write(GPIOE, (GPIO_ReadOutputData(GPIOE)^0xFFFF));
+		}
+	}
+}
